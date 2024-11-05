f30dc858d33d495c9ae49811f392b61fcf575499
Inon Cohen <96375193+Inoncohen2@users.noreply.github.com>
2024-11-05 13:57:09 +0200

    Update balloon_detector.py

--- balloon_detector.py
+++ balloon_detector.py
@@ -1,18 +1,19 @@
+import os
+import argparse
 import cv2
 import numpy as np
 import time
 from threading import Thread
 import importlib.util
-import os
-import argparse
 
-# Video stream class
 class VideoStream:
     def __init__(self, resolution=(640, 480), framerate=30):
         self.stream = cv2.VideoCapture(0)
         self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         self.stream.set(3, resolution[0])
         self.stream.set(4, resolution[1])
+        self.stream.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.stream.set(cv2.CAP_PROP_FPS, framerate)
         
         (self.grabbed, self.frame) = self.stream.read()
         self.stopped = False
@@ -39,8 +40,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--modeldir', help='Folder the .tflite file is located in', default='Balloon_TFLite_model')
 parser.add_argument('--graph', help='Name of the .tflite file', default='detect.tflite')
 parser.add_argument('--labels', help='Name of the labelmap file', default='labelmap.txt')
-parser.add_argument('--threshold', help='Minimum confidence threshold', default=0.5)
-parser.add_argument('--resolution', help='Desired webcam resolution in WxH', default='800x600')
+parser.add_argument('--threshold', help='Minimum confidence threshold', default=0.7)
+parser.add_argument('--resolution', help='Desired webcam resolution in WxH', default='1280x720')
 parser.add_argument('--edgetpu', help='Use Edge TPU Accelerator', action='store_true')
 
 args = parser.parse_args()
@@ -78,7 +79,8 @@ if labels[0] == '???':
     del(labels[0])
 
 if use_TPU:
-    interpreter = Interpreter(model_path=PATH_TO_CKPT, experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+    interpreter = Interpreter(model_path=PATH_TO_CKPT, 
+                            experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
 else:
     interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
@@ -91,13 +93,21 @@ floating_model = (input_details[0]['dtype'] == np.float32)
 input_mean = 127.5
 input_std = 127.5
 
+outname = output_details[0]['name']
+if 'StatefulPartitionedCall' in outname:
+    boxes_idx, classes_idx, scores_idx = 1, 3, 0
+else:
+    boxes_idx, classes_idx, scores_idx = 0, 1, 2
+
 # Initialize video stream
 videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
 time.sleep(1)
 
 try:
     while True:
-        frame = videostream.read()
+        frame1 = videostream.read()
+        frame = frame1.copy()
+        
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb, (width, height))
         input_data = np.expand_dims(frame_resized, axis=0)
@@ -108,27 +118,28 @@ try:
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
 
-        boxes = interpreter.get_tensor(output_details[0]['index'])[0]
-        classes = interpreter.get_tensor(output_details[1]['index'])[0]
-        scores = interpreter.get_tensor(output_details[2]['index'])[0]
+        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
+        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
+        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
 
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
         for i in range(len(scores)):
-            if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+            if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
                 if labels[int(classes[i])] == 'balloon':
+                    # Get bounding box coordinates
                     ymin = int(max(1, (boxes[i][0] * imH)))
                     xmin = int(max(1, (boxes[i][1] * imW)))
                     ymax = int(min(imH, (boxes[i][2] * imH)))
                     xmax = int(min(imW, (boxes[i][3] * imW)))
-
-                    # Draw bounding box
-                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
                     
-                    # Display confidence
-                    label = f"{int(scores[i] * 100)}%"
-                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                    label_ymin = max(ymin, labelSize[1] + 10)
-                    cv2.rectangle(frame, (xmin, label_ymin - labelSize[1] - 10), (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255), cv2.FILLED)
-                    cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+                    # Draw bounding box
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+                    
+                    # Draw confidence label
+                    confidence = f'{int(scores[i] * 100)}%'
+                    label_y = ymin - 10 if ymin - 10 > 10 else ymin + 20
+                    cv2.putText(frame, confidence, (xmin, label_y),
+                              cv2.FONT_HERSHEY_SIMPLEX, 0.7, (10, 255, 0), 2)
 
         cv2.imshow('Balloon Detection', frame)
 
